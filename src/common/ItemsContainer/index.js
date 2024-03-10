27bb472b256a860b0a6d4ea0fbbbb94e435b2282
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-03-10 20:34:33 +0100

    change item container

--- src/common/ItemsContainer/index.js
+++ src/common/ItemsContainer/index.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export default styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: end;
+  justify-content: start;
   gap: 10px;
   padding: 10px;
   border: 1px solid black;
@@ -14,6 +14,6 @@ export default styled.div`
     padding: 10px;
     font-weight:700;
     text-align: justify;
-    flex-grow: 1;
+    flex-grow: 0;
   }
 `;
